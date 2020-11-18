f8a7b9e80e10508b575a50796cc567ca06d8ff84
Marcus Paul <mrcspaul98@gmail.com>
2020-11-18 19:56:35 +0000

    Updated so that checkboxes display a quantity number input box. Changes to each of these change the state.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,20 +9,63 @@ class App extends Component {
     this.state = {
       Items: []
     }
+
+    this.handleChangeCheckbox = this.handleChangeCheckbox.bind(this)
+    this.handleChangeNumber = this.handleChangeNumber.bind(this)
   }
 
   componentDidMount() {
     fetch("https://localhost:44394/api/Item")
       .then(response => response.json())
       .then(data => {
+        for (var i = 0; i < data.length; i++) {
+          data[i].completed = false
+          data[i].quantity = 1
+          data[i].displayQuantity = false
+        }
         this.setState ({
           Items: data
         })
       })
   }
 
+  handleChangeCheckbox(id) {
+    this.setState(prevState => {
+      const updatedItems = prevState.Items.map(item => {
+        if (item.Id === id) {
+          item.completed = !item.completed
+          item.displayQuantity = !item.displayQuantity
+        }
+        return item
+      })
+      return {
+        Items: updatedItems
+      }
+    })
+  }
+
+  handleChangeNumber(id, event) {
+    this.setState(prevState => {
+      const updatedItems = prevState.Items.map(item => {
+        if (item.Id === id) {
+          item.quantity = event.target.value
+        }
+        return item
+      })
+      return {
+        Items: updatedItems
+      }
+    })
+  }
+
   render() {
-    const shopItems = this.state.Items.map(item => <Item key = {item.Id} item = {item} />)
+    const shopItems = this.state.Items.map(item => 
+    <Item 
+      key = {item.Id} 
+      item = {item}
+      handleChangeCheckbox = {this.handleChangeCheckbox}
+      handleChangeNumber = {this.handleChangeNumber}
+    />)
     return (
       <div className="item-list">
         {shopItems}
--- my-app/src/Item.js
+++ my-app/src/Item.js
@@ -5,9 +5,30 @@ function Item(props) {
     return (
         <div className="shop-item">
             <h2>{props.item.Name}</h2>
-            <p>{props.item.Price.toLocaleString
-            ("en-US", { style: "currency", currency: "GBP"})} - {props.item.Tagline}
-            <input type = "checkbox" /></p>
+            <br />
+            <div className="item-p">
+                <span>{props.item.Price.toLocaleString
+                ("en-US", { style: "currency", currency: "GBP"})} - {props.item.Tagline}</span>
+                <input 
+                    type="checkbox" 
+                    checked={props.item.completed} 
+                    onChange={() => props.handleChangeCheckbox(props.item.Id)}
+                />
+                <label 
+                    for="quantity"
+                    className={props.item.displayQuantity ? "show" : "hide"}
+                >Quantity: </label>
+                <input 
+                    type="number" 
+                    id="quantity" 
+                    value={props.item.quantity}
+                    className={props.item.displayQuantity ? "show" : "hide"}
+                    name="quantity" 
+                    min="1" 
+                    max="99"
+                    onChange={(event) => props.handleChangeNumber(props.item.Id, event)}>
+                </input>
+            </div>
         </div>
     )
 }
